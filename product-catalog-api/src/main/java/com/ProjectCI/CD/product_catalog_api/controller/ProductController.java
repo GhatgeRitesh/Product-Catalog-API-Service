e5d21cbb718b6a3f9b78516671db06fa47edcea5
GhatgeRitesh <riteshghatge12345@gmail.com>
2025-01-12 10:17:00 +0530

    initial commit

---
+++ product-catalog-api/src/main/java/com/ProjectCI/CD/product_catalog_api/controller/ProductController.java
@@ -0,0 +1,12 @@
+package com.ProjectCI.CD.product_catalog_api.controller;
+
+import lombok.extern.java.Log;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Log
+@RequestMapping("/product")
+public class ProductController {
+
+}
---
+++ product-catalog-api/src/test/java/com/ProjectCI/CD/product_catalog_api/controllerTests/ProductControllerTest.java
@@ -0,0 +1,8 @@
+package com.ProjectCI.CD.product_catalog_api.controllerTests;
+
+import lombok.extern.java.Log;
+
+@Log
+public class ProductControllerTest {
+
+}
